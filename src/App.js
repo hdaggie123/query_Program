933772b58aac601f4cb40f14f10c51f1799b2dad
hdaggie123 <hdemny@tamu.edu>
2023-07-10 11:28:06 -0500

    Added basic search functionality to the program through use of Google Custom Search API and Custom Search Engine

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
 //import logo from './logo.svg';
 import { on } from 'events';
+import axios from 'axios';
 import React, {useState} from 'react';
 import './App.css';
 
 
 function App() {
   const [query, setQuery] = useState('');
-  const [result, setResult] = useState('');
-  const handleSubmit = (event) => {
+  const [result, setResult] = useState([]);
+  const handleSubmit = async (event) => {
     event.preventDefault(); // Prevent form submission
 
     // Retrieve form data
-    setQuery(event.target.query.value);
+    const searchQuery = event.target.query.value;
 
-    // Display results in a window below
-    const resultWindow = document.getElementById('result');
-    resultWindow.innerHTML = 'Question: ' + query;
+    try {
+      // Make an HTTP GET request to the Google Custom Search JSON API
+      const response = await axios.get(
+        `https://www.googleapis.com/customsearch/v1?key=AIzaSyASsZhFnPSGdCL4ia20M4FeZWfopbu8gH0&cx=b70849ddcb9c445be&q=${encodeURIComponent(
+          searchQuery
+        )}`
+      );
 
-    // Send data to the backend
-    // Code to send the name and email values to the backend...
+      // Extract search results from the API response
+      const searchResults = response.data.items;
+
+      // Update the state with the search results
+      setResult(searchResults);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
   };
-  const handleChange = (event) => {
-    setQuery(event.target.value);
-  };
+  
   return (
     <>
-     <div className="App">
-        <form id="query" onSubmit={handleSubmit}>
-          <label htmlFor="query">What is your Question?</label>
-          <input type="text" id="query" name="query" />
-          <button type="submit">Submit here!</button>
-          <button type="reset">Reset here!</button>
-        </form>
-      </div>
-      <div id="result">
-        <p>Here is the answer to your question</p>
-      </div>
-    </>
+    <div className="App">
+      <form id="query" onSubmit={handleSubmit}>
+        <label htmlFor="query">What is your Question?</label>
+        <input type="text" id="query" name="query" />
+        <button type="submit">Submit here!</button>
+        <button type="reset">Reset here!</button>
+      </form>
+    </div>
+    <div id="result">
+      {result.map((item, index) => (
+        <div key={index}>
+          <h3>{item.title}</h3>
+          <a href={item.link}>{item.link}</a>
+          <p>{item.snippet}</p>
+        </div>
+      ))}
+    </div>
+  </>
   );
 }
 
